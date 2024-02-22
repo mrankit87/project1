2c5cd95d2a6a20c6e7fa41956a91d8c361a3f740
ANKIT KUMAR <160473253+mrankit87@users.noreply.github.com>
2024-02-22 15:23:18 +0530

    Add files via upload

---
+++ basic  calculator.py
@@ -0,0 +1,10 @@
+num1=int(input("enter first number :"))
+num2=int(input("enter second number :"))
+op=input("enter operator sign : ")[0]
+if op=="+":
+    print("addition of ",num1, "and",num2,"=", num1 +num2)
+elif op=="-":
+    print("substraction of",num1," and ",num2,"=",num1-num2)
+elif of=="*":
+    print("multipliction of " ,num1, " and ", num2,"=", num1 *num2)
+    
